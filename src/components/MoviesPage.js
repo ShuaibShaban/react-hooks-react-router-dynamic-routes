151b8f79ac3cebb4e82ae5b3333ee457afa51c3a
shuaib shaban <shuaib>
2023-01-23 12:41:50 +0300

    done

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -5,21 +5,19 @@ import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
   const match = useRouteMatch();
-
+  console.log(match);
   return (
     <div>
       <MoviesList movies={movies} />
 
-      {/* Adding code to show a message to the user to select a movie if they haven't yet */}
       <Route exact path={match.url}>
         <h3>Choose a movie from the list above</h3>
       </Route>
 
       <Route path={`${match.url}/:movieId`}>
-        <MovieShow movies={movies} />
+        <MovieShow movies={movies}/>
       </Route>
     </div>
   );
 }
-
 export default MoviesPage;
